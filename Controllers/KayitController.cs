4f28eaf1f479560936272e8484bb3eb7142b222b
Ergün Karakuş <35894461+ekarakus@users.noreply.github.com>
2022-03-31 11:12:49 +0300

    sunucu taraflı doğrulama

--- Controllers/KayitController.cs
+++ Controllers/KayitController.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 public class KayitController:Controller{
+   [HttpGet]
     public IActionResult Index(){
         Ogrenci m= new Ogrenci();
         m.ad="Ergün";
@@ -8,5 +9,14 @@ public class KayitController:Controller{
         m.kayitTarihi=DateTime.Now;
         //oluşturlan model view e gönderiliyor
         return View();
+    }
+    [HttpPost]
+    public IActionResult Index(Ogrenci m){
+    if(ModelState.IsValid){
+        ViewBag.sonuc="Kayıt tamamlandı";
+        return  View("Views/Kayit/Tebrikler.cshtml",m);
+    }else   {
+        return View(m);
+    }
     }
 }
